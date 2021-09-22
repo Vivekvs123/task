1c26d5f9e46cf804adcd121d70e2421104217535
Vivekvs123 <vivek.sakpal@neosoftmail.com>
2021-09-22 18:25:35 +0530

    commit

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ let screen=document.getElementById('screen');
 buttons=document.querySelectorAll('button');
 let screenValue='';
 var arr=[];
+function back() {
+    var value = document.getElementById("screen").value;
+    document.getElementById("screen").value = value.substr(0, value.length - 1);
+    screenValue= document.getElementById("screen").value;
+
+}
 for(item of buttons){
     item.addEventListener('click',(e)=>{
         buttonText=e.target.innerText;
@@ -12,13 +18,19 @@ for(item of buttons){
             screen.value = screenValue;
         }
        
-        else if(buttonText =='C'){
+        else if(buttonText =='Clear'){
             screenValue = "";
             screen.value = screenValue;    
         }
         else if(buttonText =='='){
+            let x= screen.value;
+            let hisexp = x;
             screen.value=eval(screenValue)
+             
+              let y = eval(screenValue);
+              let hiseval = y;
               
+              history(hisexp,hiseval);
                 
         }
        
@@ -28,4 +40,23 @@ for(item of buttons){
         }
 
     })
+}
+
+function history(x,y){
+    var task = x +" = "+y
+    console.log(task)
+    arr.push(task);
+    var list=document.getElementById('history');
+    var li = document.createElement('li');
+    li.className='a'
+    li.id='li'
+    arr.forEach(function(val,ind){
+        li.innerHTML=val
+        list.appendChild(li);
+    })
+}
+function clearHist(){
+    var task = document.getElementById('history');
+    var li = document.getElementById('li')
+    task.removeChild(li)
 }
